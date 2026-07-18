fa6468895a16cbfd5071675d2f96b8976ca5d015
NamTruong2003 <truongnamvxtd91@gmail.com>
2026-07-18 15:16:55 +0700

    feat: add app database

---
+++ app/src/main/java/com/healthtracker/data/local/AppDatabase.kt
@@ -0,0 +1,35 @@
+package com.healthtracker.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.healthtracker.data.local.dao.ActivityDao
+import com.healthtracker.data.local.dao.ActivityTypeDao
+import com.healthtracker.data.local.dao.FoodDao
+import com.healthtracker.data.local.dao.MealDao
+import com.healthtracker.data.local.dao.UserProfileDao
+import com.healthtracker.data.local.entity.ActivityEntryEntity
+import com.healthtracker.data.local.entity.ActivityTypeEntity
+import com.healthtracker.data.local.entity.FoodEntity
+import com.healthtracker.data.local.entity.MealEntryEntity
+import com.healthtracker.data.local.entity.UserProfileEntity
+
+@Database(
+    entities = [
+        UserProfileEntity::class,
+        FoodEntity::class,
+        MealEntryEntity::class,
+        ActivityTypeEntity::class,
+        ActivityEntryEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userProfileDao(): UserProfileDao
+    abstract fun foodDao(): FoodDao
+    abstract fun mealDao(): MealDao
+    abstract fun activityTypeDao(): ActivityTypeDao
+    abstract fun activityDao(): ActivityDao
+}
